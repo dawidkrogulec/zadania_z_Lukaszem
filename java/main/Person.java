947f27d591ae3e2b0f528f241dba5c3081150796
Dawid Krogulec <Dawid.Krogulec@acoustic.co>
2022-05-11 13:29:39 +0200

    Added setters, getters, constructor

---
+++ java/main/Person.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+}
