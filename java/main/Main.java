172dc7477f8daca4ba8970b0e5b0c003baa3094d
Dawid Krogulec <Dawid.Krogulec@acoustic.co>
2022-05-11 14:22:18 +0200

    Added classs, setters, getters, constructor

--- java/main/Main.java
+++ java/main/Main.java
@@ -2,5 +2,9 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Elo Dawid");
+
+        Person person = new Person("Lucas", 25);
     }
+
+
 }
